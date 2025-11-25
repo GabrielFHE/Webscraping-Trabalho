5b1e8f08c725976eeebce2aec64c4e1161c3fc74
Gabriel Freitas <128260611+GabrielFHE@users.noreply.github.com>
2025-11-25 17:10:50 -0300

    Add files via upload

---
+++ Trabalho.js
@@ -0,0 +1,30 @@
+
+function criar(doc){
+        let noticia = doc.querySelectorAll("._evg")
+        let ul = document.createElement("ul")
+        noticia.forEach(element =>{
+            let li = document.createElement("li")
+            li.innerHTML = element.innerHTML
+            ul.appendChild(li)
+        })
+        document.body.appendChild(ul)
+    }
+
+function scrap(){
+    fetch("https://proxy.corsfix.com/?https://ge.globo.com/").then(response => {
+        if(!response.ok){
+            throw new Error("Chamada HTTP Falhou")
+        }
+        return response.text()
+    }).then(dados =>{
+        let parser = new DOMParser()
+        let doc = parser.parseFromString(dados, "text/html")
+        criar(doc)
+    }).catch(erro => alert(erro))
+}
+function popularEvento(){
+    document.querySelector("#btn").addEventListener("click", evt =>{
+        scrap()
+    })
+}
+window.onload = popularEvento
